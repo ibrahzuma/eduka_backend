cb8d857c0561a2f3d503cddb3cd41005cdbf0364
unknown <ibrahimcadabra1@gmail.com>
2025-12-17 16:42:26 +0300

    API: Expose /api/dashboard/pricing/ endpoint

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DashboardSummaryView, SuperUserDashboardView
+from .views import DashboardSummaryView, SuperUserDashboardView, PricingAPIView
 
 urlpatterns = [
     path('summary/', DashboardSummaryView.as_view(), name='dashboard-summary'),
+    path('pricing/', PricingAPIView.as_view(), name='api_pricing'),
     path('superuser/', SuperUserDashboardView.as_view(), name='superuser_dashboard'),
 ]
