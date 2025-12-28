777e212efce4bd71aaacc52170c07614c5b7eb16
unknown <ibrahimcadabra1@gmail.com>
2025-12-28 13:03:56 +0300

    Add remote fix script

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import DashboardSummaryView, SuperUserDashboardView, PricingAPIView
+from .views import (
+    DashboardSummaryView, SuperUserDashboardView, PricingAPIView,
+    NotificationListAPIView, NotificationMarkReadAPIView
+)
 
 urlpatterns = [
     path('summary/', DashboardSummaryView.as_view(), name='dashboard-summary'),
     path('pricing/', PricingAPIView.as_view(), name='api_pricing'),
     path('superuser/', SuperUserDashboardView.as_view(), name='superuser_dashboard'),
+    path('notifications/', NotificationListAPIView.as_view(), name='api_notifications'),
+    path('notifications/mark-read/', NotificationMarkReadAPIView.as_view(), name='api_notifications_mark_read'),
 ]
