b10ece0d583ba1a1ab58f5c74bd3f36ba397962b
unknown <ibrahimcadabra1@gmail.com>
2025-12-28 16:07:24 +0300

    Add remote fix script

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -5,6 +5,6 @@ from . import api_views
 urlpatterns = [
     path('initiate-payment/', views.InitiatePaymentView.as_view(), name='initiate_payment'),
     path('check-status/<int:payment_id>/', views.CheckPaymentStatusView.as_view(), name='check_payment_status'),
-    path('plans/', api_views.SubscriptionPlanListView.as_view(), name='api_sub_plans'),
-    path('status/', api_views.SubscriptionStatusAPIView.as_view(), name='api_sub_status'),
+    path('api/plans/', api_views.SubscriptionPlanListView.as_view(), name='api_sub_plans'),
+    path('api/status/', api_views.SubscriptionStatusAPIView.as_view(), name='api_sub_status'),
 ]
